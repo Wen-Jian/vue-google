510fd1088296450d2ad5b739d0302b4e5dae3fe1
曾文建 <wen@cengwenjiande-MacBook-Pro.local>
2019-12-30 22:24:27 +0800

    try to call getBasicProfile

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -39,9 +39,10 @@ export default {
     }
   },
   methods: {
-    onSignInSuccess () {
+    onSignInSuccess (googleUser) {
       // `googleUser` is the GoogleUser object that represents the just-signed-in user.
       // See https://developers.google.com/identity/sign-in/web/reference#users
+      console.log(googleUser.getBasicProfile())
     },
     onSignInError () {
       // `error` contains any error occurred.
